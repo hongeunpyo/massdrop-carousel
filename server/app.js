728c9dd0d1a5eec0db3826baa902872c2e01ea35
hongeunpyo <eunpyohong@gmail.com>
2019-03-22 16:43:17 -0400

    added images for documentation

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.get('*.js', function (req, res, next) {
-  console.log('js requested')
+  console.log('js requested');
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
   res.set('Content-Type', 'text/javascript');
